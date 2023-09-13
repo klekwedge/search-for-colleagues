75587159e042efc41d89a6b0146abaa8472d41a8
Andrey <antonov.andrey.10101@gmail.com>
2023-09-13 10:18:46 +0300

    feat: add Header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <input type="text" v-model="searchTerm" @input="searchUsers" placeholder="Search users">
+  <div class="container">
+    <Header />
+    <input
+      type="text"
+      v-model="searchTerm"
+      @input="searchUsers"
+      placeholder="Search users"
+    />
     <ul>
       <li v-for="user in users" :key="user.id">{{ user.name }}</li>
     </ul>
@@ -8,29 +14,44 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
+import Header from "./components/Header.vue";
 
 export default {
+  components: {
+    Header,
+  },
   data() {
     return {
-      searchTerm: '',
+      searchTerm: "",
       users: [],
     };
   },
   methods: {
     searchUsers() {
-      axios.get('https://jsonplaceholder.typicode.com/users', {
-        params: {
-          name_like: this.searchTerm,
-        },
-      })
-        .then(response => {
+      axios
+        .get("https://jsonplaceholder.typicode.com/users", {
+          params: {
+            name_like: this.searchTerm,
+          },
+        })
+        .then((response) => {
           this.users = response.data;
         })
-        .catch(error => {
+        .catch((error) => {
           console.error(error);
         });
     },
   },
 };
 </script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  max-width: 1260px;
+  padding: 0px 20px;
+  margin: 0 auto;
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="header">
+    <h1 class="header__title">Жилфонд</h1>
+    <h2 class="header__subtitle">Пользователь</h2>
+  </div>
+</template>
+  
+  <script>
+export default {
+    name: 'Header'
+};
+</script>
+
+  <style lang="scss" scoped>
+.header {
+  display: flex;
+  width: 100%;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 40px;
+
+  .header__title {
+    color: #e31f24;
+    font-size: 32px;
+    font-weight: 700;
+  }
+
+  .header__subtitle {
+    color: #333;
+    font-size: 16px;
+    font-weight: 400;
+  }
+}
+</style>
