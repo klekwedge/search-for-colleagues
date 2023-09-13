9b136434d6e8aaef084d3233ec895b288f548571
Andrey <antonov.andrey.10101@gmail.com>
2023-09-13 18:05:07 +0300

    feat: add typing for the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,38 +1,50 @@
 import axios from 'axios';
-import Vuex from 'vuex';
+import { createStore } from 'vuex'
+import { IUser } from '../types';
 
-const store = new Vuex.Store({
+interface State {
+    users: IUser[];
+    currentUser: IUser | null
+}
+
+const store = createStore<State>({
     state: {
         users: [],
         currentUser: null,
+        // isLoading: 'idle'
     },
     mutations: {
-        SET_USERS(state, users) {
+        SET_USERS(state: State, users: IUser[]) {
             state.users = users
         },
-        SET_CURRENT_USER(state, userId) {
-            state.currentUser = state.users.find(user => user.id === userId)
-        }
+        SET_CURRENT_USER(state: State, userId: number) {
+            state.currentUser = state.users.find(user => user.id === userId) || null
+        },
+        // SET_LOADING(state, value) {
+        //     state.isLoading = value
+        // }
     },
     actions: {
-        fetchUsers({ commit }, inputValue) {
+        fetchUsers({ commit }: { commit: Function }, inputValue: string) {
+            // commit('SET_LOADING', 'loading')
+
             return axios.get('https://jsonplaceholder.typicode.com/users', {
                 params: {
                     name_like: inputValue
                 }
             }).then(response => response.data).then(users => {
                 commit('SET_USERS', users)
-            })
+            }).catch(e => console.log(e))
         },
-        changeCurrentUser({ commit }, userId) {
+        changeCurrentUser({ commit }: { commit: Function }, userId: number) {
             commit('SET_CURRENT_USER', userId)
         }
     },
     getters: {
-        getUsers(state) {
+        getUsers(state: State) {
             return state.users
         },
-        getCurrentUser(state) {
+        getCurrentUser(state: State) {
             return state.currentUser
         }
     }
